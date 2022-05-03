a473c9fd6bda85ca8557a269be7821438c169041
Will <yzj_000@163.com>
2022-05-03 21:49:02 +0800

    Rename BowlingTest to match its test subject

--- src/test/java/net/will/tdd/BowlingGameTest.java
+++ src/test/java/net/will/tdd/BowlingGameTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  *
  * <p>Test-Driven Development (TDD), Part 2
  */
-public class BowlingTest {
+public class BowlingGameTest {
     private BowlingGame bowlingGame;
 
     @BeforeEach
